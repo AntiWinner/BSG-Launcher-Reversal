02a8b9ce416451caf99dfa4e46c56ca9b8251f64
AntiWinner <108972337+AntiWinner@users.noreply.github.com>
2023-06-09 06:41:40 -0400

    Start

---
+++ Bsg.Launcher/HResult.cs
@@ -0,0 +1,8 @@
+namespace Bsg.Launcher;
+
+public static class HResult
+{
+	public const int ThereIsNotEnoughSpaceOnTheDisk = -2147024784;
+
+	public const int TheFileIsUsedByAnotherProcess = -2147024864;
+}
---
+++ Bsg.Launcher/HumanReadableSizeExtensions.cs
@@ -0,0 +1,33 @@
+namespace Bsg.Launcher;
+
+public static class HumanReadableSizeExtensions
+{
+	public static string ToHumanReadableSize(this int size)
+	{
+		return ((double)size).ToHumanReadableSize();
+	}
+
+	public static string ToHumanReadableSize(this long size)
+	{
+		return ((double)size).ToHumanReadableSize();
+	}
+
+	public static string ToHumanReadableSize(this double size)
+	{
+		string[] array = new string[5]
+		{
+			_E05B._E000(2235),
+			_E05B._E000(2233),
+			_E05B._E000(2238),
+			_E05B._E000(2179),
+			_E05B._E000(2176)
+		};
+		int num = 0;
+		while (size >= 1024.0 && num < array.Length - 1)
+		{
+			num++;
+			size /= 1024.0;
+		}
+		return string.Format(string.Format(_E05B._E000(2181), size, array[num]));
+	}
+}
